dcd18bb1a3c53d00b4228e0d9dbbbe059fdffbd9
AWinn13 <anthonymichaelwinn@gmail.com>
2023-04-05 16:26:11 -0400

    init commit

---
+++ ASP.NETCore/DojoSurveyValidations/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyValidations.Models;
+
+namespace DojoSurveyValidations.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View("index");
+    }
+
+    [HttpPost("process")]
+    public IActionResult Process(Survey survey)
+    {
+        HttpContext.Session.SetString("Name", $"{survey.Name}");
+        HttpContext.Session.SetString("Location", $"{survey.Location}");
+        HttpContext.Session.SetString("Language", $"{survey.Language}");
+        HttpContext.Session.SetString("Comment", $"{survey.Comment}");
+        Console.WriteLine(survey.Comment);
+        return RedirectToAction ("Display");
+    }
+    
+    [HttpGet("display")]
+    public IActionResult Display(Survey survey)
+    {
+        
+        return View("display");
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ASP.NETCore/DojoSurveyValidations/Models/Survey.cs
@@ -0,0 +1,11 @@
+#pragma warning disable CS8618
+namespace DojoSurveyValidations.Models;
+
+public class Survey 
+{
+    public string Name {get;set;}
+    public string Location {get;set;}
+    public string Language {get;set;}
+    public string Comment {get;set;}
+    
+}
